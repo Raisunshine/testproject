885fd0adc664946e21e722ab015fc609c4efbae5
Raisunshine <naialligator@outlook.com>
2023-02-03 00:27:40 -0500

    modified: main.css modified; main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const ITEMS_CONTAINER = document.getElementById("items");
-const ITEMS_TEMPLATE = document.getElementById("itemTemplate");
+const ITEM_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
 const ADD_BUTTON = document.getElementById("add")
 
 let items = getItem();
